611238f83ace8db8980acd8d513766beab79e20b
Mohammad Ahmad <ma3571318z@gmail.com>
2021-05-16 00:45:09 +0300

    login done

--- controllers/admin.ts
+++ controllers/admin.ts
@@ -1,74 +1,69 @@
-import express, {Request, Response, NextFunction} from "express";
-import {con} from "../config/db";
+import express, { Request, Response, NextFunction } from "express";
+import { con } from "../config/db";
 import signAdminJWT from "../helpers/signAdminJWT";
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 
-const login = (req: Request, res: Response, next: NextFunction) =>{
-    console.log(req.body);
-    let {email, password} = req.body;
-    let query = `SELECT ID, email, password FROM admin WHERE email='${email}' AND password='${password}'`;
+const login = (req: Request, res: Response, next: NextFunction) => {
+  let { email, password } = req.body;
+  let query = `SELECT ID, email, password FROM admin WHERE email='${email}' AND password='${password}'`;
 
-    let cookies  = new Cookies();
+  let cookies = new Cookies();
 
-    con.query(query, (error:Error, results: any, fields: any) =>{
-        if(error) {
-            return res.status(400).json({
-                success: false,
-                message: 'الايميل او كلمة السر خطأ',
-                error: error
-            })
-        }
-        signAdminJWT(results[0], (_error, token) =>{
-            if(_error){
-                return res.status(500).json({
-                    success: false,
-                    message: 'الايميل او كلمة السر خطأ',
-                    error: _error
-                })
-            } else if(token){
-                cookies.set('token', token, {
-                    expires: new Date(0),
-                    path: '/',
-                    domain: 'http://localhost:5500',
-                    httpOnly : false,
-                    secure: false
-                })
-                return res.status(200).json({
-                    success: true,
-                    message: 'تم تسجيل الدخول بنجاح',
-                    token
-                })
-            }
-        })
-    })
-};
-
-const updateAdmin = (req: Request, res: Response, next: NextFunction) =>{
-    let {email, password} = req.body;
-    let {id} = req.params;
-
-    let query = `UPDATE admin SET email="${email}", password="${password}" WHERE ID=${id}`;
-
-    con.query(query, (error:Error, results: any, fields: any) => {
-        if(error) {
-            return res.status(400).json({
-                success: false,
-                message: 'حدث خطأ, يرجى المحاولة فيما بعد',
-                error: error
-            })
-        } else if(results) {
-            return res.status(200).json({
-                success: true,
-                message: 'تم التعديل بنجاح',
-            });
-        }
+  con.query(query, (error: Error, results: any, fields: any) => {
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: "الايميل او كلمة السر خطأ",
+        error: error,
+      });
+    }
+    signAdminJWT(results[0], (_error, token) => {
+      if (_error) {
+        return res.status(500).json({
+          success: false,
+          message: "الايميل او كلمة السر خطأ",
+          error: _error,
+        });
+      } else if (token) {
+        cookies.set("token", token, {
+          expires: new Date(0),
+          path: "/",
+          domain: "http://localhost:5500",
+          httpOnly: false,
+          secure: false,
+        });
+        return res.status(200).json({
+          success: true,
+          message: "تم تسجيل الدخول بنجاح",
+          token,
+        });
+      }
     });
-
+  });
 };
 
-const acceptDealer = (req: Request, res: Response, next: NextFunction) =>{
-    
+const updateAdmin = (req: Request, res: Response, next: NextFunction) => {
+  let { email, password } = req.body;
+  let { id } = req.params;
+
+  let query = `UPDATE admin SET email="${email}", password="${password}" WHERE ID=${id}`;
+
+  con.query(query, (error: Error, results: any, fields: any) => {
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: "حدث خطأ, يرجى المحاولة فيما بعد",
+        error: error,
+      });
+    } else if (results) {
+      return res.status(200).json({
+        success: true,
+        message: "تم التعديل بنجاح",
+      });
+    }
+  });
 };
 
+const acceptDealer = (req: Request, res: Response, next: NextFunction) => {};
 
-export default {login, updateAdmin, acceptDealer};
+export default { login, updateAdmin, acceptDealer };
--- server.ts
+++ server.ts
@@ -3,52 +3,51 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import connection from "./config/db";
-import {adminRouter} from "./routes/admin";
-import {sliderRouter} from "./routes/slider";
-import {dealerRouter} from "./routes/dealer";
-import {userRouter} from "./routes/user";
-import {cartRouter} from "./routes/cart";
-import {creditRouter} from "./routes/credit_card";
-import {productRouter} from "./routes/product";
-import {TransitionRouter} from "./routes/transition";
-import {TransitionPricingRouter} from "./routes/transition_pricing";
+import { adminRouter } from "./routes/admin";
+import { sliderRouter } from "./routes/slider";
+import { dealerRouter } from "./routes/dealer";
+import { userRouter } from "./routes/user";
+import { cartRouter } from "./routes/cart";
+import { creditRouter } from "./routes/credit_card";
+import { productRouter } from "./routes/product";
+import { TransitionRouter } from "./routes/transition";
+import { TransitionPricingRouter } from "./routes/transition_pricing";
 
 const app = express();
 
 app.use(cors());
 app.use(helmet());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
 
-io.on("connection", function(socket: any) {
+io.on("connection", function (socket: any) {
   console.log("a user connected");
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 
-  socket.on('chat message', (msg: string) => {
-    console.log('message: ' + msg);
+  socket.on("chat message", (msg: string) => {
+    console.log("message: " + msg);
   });
-
 });
 
 connection;
 
-app.use('/uploads', express.static('uploads'));
-app.use(express.urlencoded({extended: false}));
+app.use("/uploads", express.static("uploads"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/', userRouter);
-app.use('/', adminRouter);
-app.use('/', dealerRouter);
-app.use('/', sliderRouter);
-app.use('/', cartRouter);
-app.use('/', creditRouter);
-app.use('/', productRouter);
-app.use('/', TransitionRouter);
-app.use('/', TransitionPricingRouter);
+app.use("/", userRouter);
+app.use("/", adminRouter);
+app.use("/", dealerRouter);
+app.use("/", sliderRouter);
+app.use("/", cartRouter);
+app.use("/", creditRouter);
+app.use("/", productRouter);
+app.use("/", TransitionRouter);
+app.use("/", TransitionPricingRouter);
 
-http.listen(4000, () => console.log(`server is running at port 4000`))
+http.listen(4000, () => console.log(`server is running at port 4000`));
