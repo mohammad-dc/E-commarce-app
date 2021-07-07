ae043400c53233545b06ae1cfe665a1a75890717
Mohammad Ahmad <ma3571318z@gmail.com>
2021-07-07 13:19:11 +0300

    deploy on heroku

--- build/routes/customer.js
+++ build/routes/customer.js
@@ -10,18 +10,19 @@ var extractAdminJWT_1 = __importDefault(require("../helpers/extractAdminJWT"));
 var extractCustomerJWT_1 = __importDefault(require("../helpers/extractCustomerJWT"));
 var extractRequest_1 = require("../helpers/extractRequest");
 var user_1 = require("../validations/user");
+var uploadImage_1 = require("../helpers/uploadImage");
 exports.customerRouter = express_1.default.Router();
 //customer
 exports.customerRouter.get("/api/v1/user/customer/verify", extractCustomerJWT_1.default, customer_1.default.verifyLogin);
 exports.customerRouter.post("/api/v1/user/customer/auth/register", extractRequest_1.extractRequest(user_1.userSignupSchema), customer_1.default.registerUser);
 exports.customerRouter.post("/api/v1/user/customer/auth/login", extractRequest_1.extractRequest(user_1.userLoginSchema), customer_1.default.loginUser);
 exports.customerRouter.put("/api/v1/user/customer/update/:id", extractCustomerJWT_1.default, function (req, res, next) {
-    res.status(400).json({ success: Object.keys(req.body).length === 0 });
-    if (req.file) {
-        res.status(400).json({ success: true });
+    if (Object.keys(req.body).length === 0) {
+        uploadImage_1.upload.single("image");
+        next();
     }
     else {
-        res.status(400).json(req.body);
+        next();
     }
 }, extractRequest_1.extractRequest(user_1.userUpdateSchema), customer_1.default.updateUser);
 exports.customerRouter.get("/api/v1/user/customer/get/:id", extractCustomerJWT_1.default, customer_1.default.retrieveUser);
--- routes/customer.ts
+++ routes/customer.ts
@@ -32,11 +32,11 @@ customerRouter.put(
   "/api/v1/user/customer/update/:id",
   extractCustomerJWT,
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.status(400).json({ success: Object.keys(req.body).length === 0 });
-    if (req.file) {
-      res.status(400).json({ success: true });
+    if (Object.keys(req.body).length === 0) {
+      upload.single("image");
+      next();
     } else {
-      res.status(400).json(req.body);
+      next();
     }
   },
   extractRequest(userUpdateSchema),
