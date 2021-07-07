0317c73f0c123fe5557bdf8ee0d7e4829b312ff4
Mohammad Ahmad <ma3571318z@gmail.com>
2021-07-07 12:55:13 +0300

    deploy on heroku

--- routes/customer.ts
+++ routes/customer.ts
@@ -32,7 +32,7 @@ customerRouter.put(
   "/api/v1/user/customer/update/:id",
   extractCustomerJWT,
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.status(400).json({ success: typeof req.file === "undefined" });
+    res.status(400).json({ success: typeof req.body });
     if (req.file) {
       res.status(400).json({ success: true });
     } else {
